377892effca4883158340245304098ef8b5597b1
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-23 13:28:24 -0400

    0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Board.Cache.MixProject do
   def project do
     [
       app: :islands_board_cache,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Board Cache",
