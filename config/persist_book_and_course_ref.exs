2dbddd318c9c60cb0ecca8fda80c93662d65bd97
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-07 20:05:29 -0500

    0.1.10

--- config/persist_book_and_course_ref.exs
+++ config/persist_book_and_course_ref.exs
@@ -1,17 +1,19 @@
 import Config
 
-book_ref =
-  """
-  Inspired by the book [Functional Web Development]
-  (https://pragprog.com/book/lhelph/functional-web-development-
-  with-elixir-otp-and-phoenix) by Lance Halvorsen.
-  """
-  |> String.replace("\n", "")
+config :islands_board_cache,
+  book_ref:
+    """
+    Inspired by the book [Functional Web Development]
+    (https://pragprog.com/book/lhelph/functional-web-development-
+    with-elixir-otp-and-phoenix) by Lance Halvorsen.
+    """
+    |> String.replace("\n", "")
 
-course_ref =
-  """
-  Also inspired by the course [Multi-Player Bingo]
-  (https://pragmaticstudio.com/courses/unpacked-bingo)\s
-  by Mike and Nicole Clark.
-  """
-  |> String.replace("\n", "")
+config :islands_board_cache,
+  course_ref:
+    """
+    Also inspired by the course [Multi-Player Bingo]
+    (https://pragmaticstudio.com/courses/unpacked-bingo)\s
+    by Mike and Nicole Clark.
+    """
+    |> String.replace("\n", "")
